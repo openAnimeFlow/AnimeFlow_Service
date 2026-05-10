cb87bd52b4961c6adcd9b4a5894032f3e3d2fd1b
LiggMax <wenzhouli06@gmail.com>
2026-05-10 23:43:53 +0800

    feat(db): 添加数据库结构

---
+++ db/anime_flow.sql
@@ -0,0 +1,57 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80043 (8.0.43)
+ Source Host           : localhost:3306
+ Source Schema         : anime_flow
+
+ Target Server Type    : MySQL
+ Target Server Version : 80043 (8.0.43)
+ File Encoding         : 65001
+
+ Date: 10/05/2026 23:43:11
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for danmaku
+-- ----------------------------
+DROP TABLE IF EXISTS `danmaku`;
+CREATE TABLE `danmaku`  (
+  `id` bigint NOT NULL COMMENT '主键ID',
+  `message` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '弹幕内容',
+  `time` double NOT NULL COMMENT '弹幕时间（秒）',
+  `type` tinyint NOT NULL COMMENT '弹幕类型 1=普通 4=底部 5=顶部',
+  `color` int NOT NULL COMMENT '弹幕颜色（ARGB整数）',
+  `source` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '弹幕来源',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_time`(`time` ASC) USING BTREE,
+  INDEX `idx_type`(`type` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '弹幕表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for forum_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `forum_comment`;
+CREATE TABLE `forum_comment`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL COMMENT '用户id(一般时bangumi id)',
+  `parent_id` bigint NOT NULL DEFAULT 0 COMMENT '父评论ID；0表示一级评论，非0表示回复某条评论（楼中楼）',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '评论内容',
+  `image_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片地址',
+  `like_count` int NOT NULL DEFAULT 0 COMMENT '评论点赞数量',
+  `ip_address` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '发表评论时的IP地址',
+  `create_time` datetime NOT NULL COMMENT '评论创建时间',
+  `nickname` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `avatar_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `forum_comment_parent_id_index`(`parent_id` ASC) USING BTREE,
+  INDEX `forum_comment_user_id_index`(`user_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '论坛评论' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
