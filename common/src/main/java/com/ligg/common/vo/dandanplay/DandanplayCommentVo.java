f929891c5d0fdce367987ca7ac1aecf387663598
LiggMax <wenzhouli06@gmail.com>
2026-05-12 19:13:21 +0800

    style(vo): 优化DandanplayCommentVo代码注释格式
    - 在DanmakuVo记录中添加注释，说明p字段含义

--- common/src/main/java/com/ligg/common/vo/dandanplay/DandanplayCommentVo.java
+++ common/src/main/java/com/ligg/common/vo/dandanplay/DandanplayCommentVo.java
@@ -6,6 +6,9 @@ public record DandanplayCommentVo(Integer count, List<DanmakuVo> comments) {
 
     public record DanmakuVo(
             Long cid,
+            /*
+             * 出现时间,弹幕类型,颜色,平台
+             */
             String p,
             String m) {
     }
