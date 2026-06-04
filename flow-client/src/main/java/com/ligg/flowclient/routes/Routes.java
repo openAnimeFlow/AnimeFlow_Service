9c10ef52c84e9b6fb1ddcb999d69ce2ccc7917d5
LiggMax <wenzhouli06@gmail.com>
2026-06-05 04:52:05 +0800

    docs(routes): 更新页面路由类注释文档

--- flow-client/src/main/java/com/ligg/flowclient/routes/Routes.java
+++ flow-client/src/main/java/com/ligg/flowclient/routes/Routes.java
@@ -1,3 +1,8 @@
+/**
+ * @author Ligg
+ * @create_time 2025/12/23 12:42
+ * @update_time 2025/12/23 12:42
+ **/
 package com.ligg.flowclient.routes;
 
 import org.springframework.stereotype.Controller;
@@ -5,10 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author Ligg
- * @create_time 2025/12/23 12:42
- * @update_time 2025/12/23 12:42
- **/
+ * 页面路由
+ */
 @Controller
 @RequestMapping("/api")
 public class Routes {
