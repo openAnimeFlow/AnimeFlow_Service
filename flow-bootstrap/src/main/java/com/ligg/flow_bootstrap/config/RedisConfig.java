ea722567d11c2212e021f56752f9153c862d0b3a
LiggMax <wenzhouli06@gmail.com>
2026-08-29 16:10:55 +0800

    move(config): 将Redis配置从flow-client移动到flow-bootstrap

--- flow-bootstrap/src/main/java/com/ligg/flow_bootstrap/config/RedisConfig.java
+++ flow-bootstrap/src/main/java/com/ligg/flow_bootstrap/config/RedisConfig.java
@@ -1,12 +1,12 @@
-package com.ligg.flowclient.config;
+package com.ligg.flow_bootstrap.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -17,7 +17,9 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
- * Redis配置类
+ * 应用级 Redis 配置。
+ *
+ * <p>Redis 被多个业务模块使用，因此配置放在统一启动模块，而不是某个业务模块中。</p>
  */
 @Configuration
 public class RedisConfig {
@@ -27,13 +29,9 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
 
-        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
         ObjectMapper mapper = new ObjectMapper();
-
-        // 添加Java 8时间类型支持
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         mapper.activateDefaultTyping(
                 LaissezFaireSubTypeValidator.instance,
@@ -42,13 +40,10 @@ public class RedisConfig {
         );
 
         Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(mapper, Object.class);
-
-        // 使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(serializer);
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(serializer);
-
         template.afterPropertiesSet();
         return template;
     }
