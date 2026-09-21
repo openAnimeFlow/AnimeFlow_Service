bdc47ddc03bbbe9a8d8fb486338aecbc341566ec
LiggMax <wenzhouli06@gmail.com>
2026-09-21 12:49:05 +0800

    chore: remove obsolete database migrations

--- db/migrations/20260921_collection_sync_recovery_index.sql
+++
@@ -1,3 +0,0 @@
--- Speed up recovery polling by filtering active statuses and ordering by recovery time.
-ALTER TABLE user_bgm_collection_sync_task
-    ADD INDEX idx_sync_task_recovery (status, heartbeat_at, created_at, id);
